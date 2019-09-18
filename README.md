f193fb646636f21d75d0a34858b8fe1982551b5e
Takahiro INOUE <takahiro.inoue@aist.go.jp>
2019-09-18 23:55:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# SongleJp/techbookfest-songle-api-examples
+
+## Requirements
+
+- nodejs 10.x
+- npm 6.x
+
+## How to serve
+
+```
+$ npm install
+$ npm run serve
+```
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @author Takahiro INOUE <takahiro.inoue@aist.go.jp>
+ * @license MIT
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "../node_modules/semantic-ui-css/semantic.min.css";
+
+ReactDOM.render(<h1>It works!</h1>, document.querySelector("#app"));
